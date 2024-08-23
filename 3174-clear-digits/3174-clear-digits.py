370b62b115c1a0aa1e8811fc9ff2029f8eb60b9d
JMJ <104509994+jmj617@users.noreply.github.com>
2024-08-23 09:35:33 +0900

    Time: 40 ms (40.26%), Space: 16.4 MB (92.62%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.py
+++ 3174-clear-digits/3174-clear-digits.py
@@ -1,25 +1,20 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        slist = list(s)
-        num = 0
-        for i in range(len(slist)):
-            if slist[i].isdigit():
-                num += 1
-        if num == 0:
-            return s
-        else:
-            while True:
-                slist = list(s)
-                digit_removed = False
-                for i in range(len(slist)):
-                    if slist[i].isdigit():
-                        a = slist[i-1] + slist[i]
-                        print(a)
-                        s = s.replace(a, "", 1)
-                        print(s)
-                        digit_removed = True
-                if not digit_removed:  
-                    break
+        while True:  
+            slist = list(s)
+            digit_removed = False  
+
+            for i in range(1, len(slist)):  
+                if slist[i].isdigit():
+                    a = slist[i-1] + slist[i]
+                    print(f"Removing: {a}")
+                    s = s.replace(a, "", 1)  
+                    print(f"Updated string: {s}")
+                    digit_removed = True
+                    break  
+
+            if not digit_removed:  
+                break
         return s
         
 
