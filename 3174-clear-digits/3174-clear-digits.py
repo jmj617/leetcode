88e78ad87b95842da9b77fdb26cff5dafa004224
JMJ <104509994+jmj617@users.noreply.github.com>
2024-08-23 09:40:57 +0900

    Time: 40 ms (40.26%), Space: 16.6 MB (22.04%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.py
+++ 3174-clear-digits/3174-clear-digits.py
@@ -7,9 +7,7 @@ class Solution:
             for i in range(1, len(slist)):  
                 if slist[i].isdigit():
                     a = slist[i-1] + slist[i]
-                    print(f"Removing: {a}")
                     s = s.replace(a, "", 1)  
-                    print(f"Updated string: {s}")
                     digit_removed = True
                     break  
 
