7fd7772a32e9af6a0a73d83d729047acc4f62b5c
JMJ <104509994+jmj617@users.noreply.github.com>
2024-08-28 09:11:26 +0900

    Time: 47 ms (13.23%), Space: 16.6 MB (41.08%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
@@ -2,7 +2,12 @@ class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
         answer = 0
         
-        for i in range(len(startTime)):
-            if startTime[i] <= queryTime and queryTime<=endTime[i]:
+        # for i in range(len(startTime)):
+        #     if startTime[i] <= queryTime and queryTime<=endTime[i]:
+        #         answer += 1
+        # return answer
+
+        for start, end in zip(startTime, endTime):
+            if start <= queryTime and queryTime <= end:
                 answer += 1
         return answer
