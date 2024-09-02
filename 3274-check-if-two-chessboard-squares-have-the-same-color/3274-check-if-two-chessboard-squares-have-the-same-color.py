140dfe920324fb8e826a12a03ffbc4379c07017f
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-02 09:18:01 +0900

    Time: 40 ms (100%), Space: 16.5 MB (100%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,18 +1,27 @@
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        # 알파벳 부분을 'a'를 1로 간주하여 숫자로 변환
-        col1 = ord(coordinate1[0]) - ord('a') + 1
-        col2 = ord(coordinate2[0]) - ord('a') + 1
+        # col1 = ord(coordinate1[0]) - ord('a') + 1
+        # col2 = ord(coordinate2[0]) - ord('a') + 1
         
-        # 숫자 부분은 그대로 정수로 변환
-        row1 = int(coordinate1[1])
-        row2 = int(coordinate2[1])
+        # row1 = int(coordinate1[1])
+        # row2 = int(coordinate2[1])
         
-        # 첫 번째 좌표의 색상 판별 (흑색인지 확인)
-        color1_is_black = (col1 + row1) % 2 == 0
+        # color1_is_black = (col1 + row1) % 2 == 0
         
-        # 두 번째 좌표의 색상 판별 (흑색인지 확인)
-        color2_is_black = (col2 + row2) % 2 == 0
+        # color2_is_black = (col2 + row2) % 2 == 0
         
-        # 두 좌표의 색상이 같으면 True 반환
-        return color1_is_black == color2_is_black
+        # # 두 좌표의 색상이 같으면 True 반환
+        # return color1_is_black == color2_is_black
+
+        black = ['a', 'c', 'e', 'g']
+        if (coordinate1[0] in black) == (coordinate2[0] in black):
+            # 시작 색 같을 때
+            if (int(coordinate1[1]) % 2) == (int(coordinate2[1]) % 2):
+                return True
+            else:
+                return False
+        else:
+            if (int(coordinate1[1]) % 2) != (int(coordinate2[1]) % 2):
+                return True
+            else:
+                return False
