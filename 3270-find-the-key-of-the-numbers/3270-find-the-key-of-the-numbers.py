bdfaca3170f1b26769f2965e8fb751098ab056a6
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-03 10:04:44 +0900

    Time: 23 ms (99.26%), Space: 16.5 MB (83.08%) - LeetHub

--- 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.py
+++ 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.py
@@ -4,6 +4,7 @@ class Solution:
         f_num2 = str(num2).zfill(4)
         f_num3 = str(num3).zfill(4)
         answer = ''
-        for n1, n2, n3 in zip(f_num1, f_num2, f_num3):
-            answer += str(min(int(n1), int(n2), int(n3)))
+        for chr_tuple in zip(f_num1, f_num2, f_num3):
+            answer += str(min(chr_tuple))
+            
         return int(answer)
