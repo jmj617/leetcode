d79e1b82d9e319793b64d5b1778a272f3810c83e
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-05 09:36:48 +0900

    Time: 203 ms (5.16%), Space: 28.6 MB (9.79%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -1,24 +1,20 @@
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        # import numpy as np
-        # mat = np.zeros((m, n))
+        import numpy as np
+        mat = np.zeros((m, n))
         answer = 0
-        # for x, y in indices:
-        #     mat[:, y] += 1
-        #     mat[x] += 1
-        # for i in range(m):
-        #     for j in range(n):
-        #         if mat[i][j] % 2 == 1:
-        #             answer += 1
-
-        mat = [[0] * n for _ in range(m)]
-        print(mat)
         for x, y in indices:
-            # x=0, y=1
-            for i in range(m):
-                mat[i][y] += 1
-            for j in range(n):
-                mat[x][j] += 1
+            mat[:, y] += 1
+            mat[x] += 1
+
+        # mat = [[0] * n for _ in range(m)]
+        # print(mat)
+        # for x, y in indices:
+        #     # x=0, y=1
+        #     for i in range(m):
+        #         mat[i][y] += 1
+        #     for j in range(n):
+        #         mat[x][j] += 1
     
         for i in range(m):
             for j in range(n):
