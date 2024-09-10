8f3509a48dba518d5ae2a87a6a0b08f363c87888
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-10 09:32:23 +0900

    Time: 37 ms (50.06%), Space: 16.6 MB (44.86%) - LeetHub

--- 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.py
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.py
@@ -1,15 +1,16 @@
 class Solution:
     def numberOfChild(self, n: int, k: int) -> int:
         children = [i for i in range(n)]
-        # a = 2 * (n - 1)
-        # if n <= (k % a):
-        #     answer = children[-2 -((k % a) - n)]
-        # else:
-        #     answer = k % (2 * (n - 1))
+        a = 2 * (n - 1)
+        if n <= (k % a):
+            answer = children[-2 -((k % a) - n)]
+        else:
+            answer = children[k % a]
+        return answer
 
-        children = [i for i in range(n)]
-        queue = children + children[1:-1][::-1]
-        x = k % len(queue)
+        # children = [i for i in range(n)]
+        # queue = children + children[1:-1][::-1]
+        # x = k % len(queue)
+        # return queue[x]
 
 
-        return queue[x]
