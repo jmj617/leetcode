a0670fcb679cd98c90784207fcddab9f68cd4dc1
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-12 09:29:38 +0900

    Time: 90 ms (29.4%), Space: 16.5 MB (98.43%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -3,11 +3,14 @@ class Solution:
         
         answer = 0
         for el in arr1:
-            valid = 0
+            valid = True
+        # arr2의 모든 요소와 비교
             for el2 in arr2:
-                if abs(el - el2) > d:
-                    valid += 1
-            if valid == len(arr2):
+                if abs(el - el2) <= d:
+                    valid = False
+                    break
+        # 모든 조건을 만족하면 distance_value를 증가
+            if valid:
                 answer += 1
 
         return answer
