2f99d48758475afb2e7ab83b461988bb33e9e80c
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-13 10:08:30 +0900

    Time: 35 ms (78.31%), Space: 16.4 MB (94.69%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -8,25 +8,22 @@ class Solution:
         #     nums = [num - minnum for num in nums if num > minnum]
         #     cnt += 1
         # return cnt
-        if len(nums) == 1:
-            if nums[0] == 0:
-                return cnt
-            else:
-                cnt += 1
-                return cnt
-        print(cnt)
-        minnum = min(x for x in nums if x > 0)
-        while any(nums):
-            nums = [num - minnum for num in nums if num > 0]
-            cnt += 1
-            print(nums)
-            if any(nums):
-                minnum = min(x for x in nums if x > 0)
-            
-        return cnt
+        
+        # if not any(nums):
+        #     return cnt
+
+        # minnum = min(x for x in nums if x > 0)
+        # while any(nums):
+        #     nums = [num - minnum for num in nums if num > 0]
+        #     cnt += 1
+        #     if any(nums):
+        #         minnum = min(x for x in nums if x > 0)
+  
+        # return cnt
 
         # elements = set()
         # for num in nums:
         #     if num > 0:
         #         elements.add(num)
         # return len(elements)
+        return len(set(num for num in nums if num > 0))
