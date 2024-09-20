7fe1b9dfc89cf26b277b149ceccffef498f5856e
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-20 09:27:50 +0900

    Time: 48 ms (59.03%), Space: 16.6 MB (6.02%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,19 +1,37 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
         
-        inc_len, dec_len = 1, 1 
-        max_len = 1
+        # inc_len, dec_len = 1, 1 
+        # max_len = 1
         
-        for i in range(1, len(nums)):
-            if nums[i] > nums[i - 1]:
-                inc_len += 1 
-                dec_len = 1
-            elif nums[i] < nums[i - 1]:
-                dec_len += 1
-                inc_len = 1
-            else:
-                inc_len = dec_len = 1
+        # for i in range(1, len(nums)):
+        #     if nums[i] > nums[i - 1]:
+        #         inc_len += 1 
+        #         dec_len = 1
+        #     elif nums[i] < nums[i - 1]:
+        #         dec_len += 1
+        #         inc_len = 1
+        #     else:
+        #         inc_len = dec_len = 1
             
-            max_len = max(max_len, inc_len, dec_len)
+        #     max_len = max(max_len, inc_len, dec_len)
         
-        return max_len
+        # return max_len
+        if len(nums) == 1:
+            answer = 1
+        inc = 1
+        dec = 1
+        answer = 1
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                inc += 1
+                dec = 1
+            elif nums[i-1] > nums[i]:
+                inc = 1
+                dec += 1
+            else:
+                inc = 1
+                dec = 1
+            answer = max(answer, inc, dec)
+        
+        return answer 
