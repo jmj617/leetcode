7d97c2f75c8ee0e0bc9e691ce0cb5ecd3a30768a
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-26 09:41:38 +0900

    Time: 36 ms (83.9%), Space: 16.5 MB (28.73%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -1,15 +1,35 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        answer = 0
+        # answer = 0
         
-        re_nums = [nums[0]]
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                re_nums.append(nums[i])
-        for i in range(1, len(re_nums) - 1):
-            if re_nums[i-1] < re_nums[i] > re_nums[i+1]:
-                answer += 1
-            elif re_nums[i-1] > re_nums[i] < re_nums[i+1]:
-                answer += 1
+        # re_nums = [nums[0]]
+        # for i in range(1, len(nums)):
+        #     if nums[i] != nums[i-1]:
+        #         re_nums.append(nums[i])
+        # for i in range(1, len(re_nums) - 1):
+        #     if re_nums[i-1] < re_nums[i] > re_nums[i+1]:
+        #         answer += 1
+        #     elif re_nums[i-1] > re_nums[i] < re_nums[i+1]:
+        #         answer += 1
 
+        # return answer
+
+        answer = 0
+        left = nums[0]
+        for i in range(1, len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                continue
+            
+            right = None
+            for j in range(i + 1, len(nums)):
+                if nums[j] != nums[i]:
+                    right = nums[j]
+                    break
+            
+            if right is not None:
+                if (left < nums[i] > right) or (left > nums[i] < right):
+                    answer += 1
+            
+            left = nums[i]
+        
         return answer
