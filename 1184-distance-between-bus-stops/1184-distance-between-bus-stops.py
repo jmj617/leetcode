2f8b6a28992fc88488f4f58610058471c2447584
JMJ <104509994+jmj617@users.noreply.github.com>
2024-09-30 09:49:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -1,31 +1,14 @@
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
         # 역방향 거리 = 전체 둘레 - 정방향 거리
-        # 0, 1, 2, 3
+
         a = 0
         b = 0
         total = sum(distance)
-
-        i = start
-
-        while i != destination:
-            a += distance[i]
-            i = (i + 1) % len(distance)
-
-        b = total - a
-        return min(a, b)
-        
-        
-        
-        
-        if start > destination:
-            start, destination = destination, start
-        a = 0
-        b = 0
-        total = sum(distance)
-        a = sum(distance[start:destination])
-        # a = (i + 1) % len(distance)
-        
-        b = total - a
+        for i in range(len(distance)):
+            if i + 1 == destination:
+                a = sum(distance[:i+1])
+                # a = (i + 1) % len(distance)
+                b = total - a
         
         return min(a, b)
