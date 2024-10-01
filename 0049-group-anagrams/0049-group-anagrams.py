4cff255bee88287e11b061f2ae7b617a5a8fe6ef
JMJ <104509994+jmj617@users.noreply.github.com>
2024-10-01 09:31:49 +0900

    Time: 85 ms (77.83%), Space: 20.3 MB (52.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,7 +4,6 @@ class Solution:
 
         for word in strs:
             word2 = ''.join(sorted(word))
-            print(word2)
             if word2 not in dic:
                 dic[word2] = []
             dic[word2].append(word)
