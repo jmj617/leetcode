1e8eebf5adfbd570d0b5de48f0945f22a2fbaa9e
JMJ <104509994+jmj617@users.noreply.github.com>
2024-10-08 09:15:49 +0900

    Time: 38 ms (38.48%), Space: 16.5 MB (30.35%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,6 +1,5 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        # slist = s.split('#')
         sdict = {}
         slist = []
         for i in range(1, 27):
@@ -8,16 +7,19 @@ class Solution:
                 sdict[str(i)] = chr(i + 96)
             else:
                 sdict[str(i)+'#'] = chr(i + 96)
+        
         j = 0
         while j < len(s):
             if j+2 < len(s) and s[j+2] == '#':
-                slist.append(s[j:j+3])
+                slist.append(sdict[s[j:j+3]])
                 j += 3
             else:
-                slist.append(s[j])
+                slist.append(sdict[s[j]])
                 j += 1
-        answer = ''
-        for ch in slist:
-            answer += sdict[ch]
+        
+        # answer = ''
+        # for ch in slist:
+        #     answer += sdict[ch]
+        print(slist)
 
-        return answer
+        return ''.join(slist)
